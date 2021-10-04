8e584b2b4aca235985b9620f8da07207846ab0c3
murraymilton <murray.l.milton@outlook.com>
2021-10-04 22:12:02 +0200

    Added default marker for POI

--- Web-Map/mapPoints1.py
+++ Web-Map/mapPoints1.py
@@ -1,4 +1,6 @@
 import folium
 map = folium.Map(location=[51.1657, 10.4515],
                  zoom_start=6, tiles="Stamen Terrain")
+map.add_child(folium.Marker(
+    location=[51.0, 10.1], popup="POI: List here", icon=folium.Icon(color="red")))
 map.save("MapDEU11.html")
